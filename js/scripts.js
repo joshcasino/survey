23637640b0cbe66d982ec421e62ce258f53d75c2
Josh Casuccio and Ryan Apking <josh@>
2016-08-10 14:37:43 -0700

    add form elements and functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,21 @@
 
 // User interface logic:
 $(document).ready(function(){
+	$("#surveyForm").submit(function(event) {
+		var name = $("#name").val();
+		var food = $("input:radio[name=food]:checked").val();
+		var music = $("#music").val();
+		$("#nameDisplay").text(name);
+		$("#foodDisplay").text(food);
+		$("#musicDisplay").text(music);
+
+		var favoriteColor = $("#color").val();
+		$(".container").css("background-color", favoriteColor);
+
+		event.preventDefault();
+
+	});
+
+
 
 });
